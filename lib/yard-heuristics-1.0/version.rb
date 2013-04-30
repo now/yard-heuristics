39f63f21337b74f78947cc239e979f2f2392e6d1
Nikolai Weibull <now@bitwi.se>
2013-04-30 13:07:26 +0200

    Update dependencies

--- lib/yard-heuristics-1.0/version.rb
+++ lib/yard-heuristics-1.0/version.rb
@@ -6,7 +6,7 @@ module YARDHeuristics
   Version = Inventory.new(1, 1, 1){
     def dependencies
       super + Inventory::Dependencies.new{
-        development 'inventory-rake', 1, 3, 0
+        development 'inventory-rake', 1, 4, 0
         development 'lookout', 3, 0, 0
         development 'lookout-rake', 3, 0, 0
         runtime 'yard', 0, 8, 0, :feature => 'yard'
